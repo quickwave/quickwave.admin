d7caddef8e4b63ac868eb571b64f00567a1fff79
Seya Obey <seyaobey@gmail.com>
2016-11-01 13:35:59 +0200

    Add project files.

---
+++ quickwave.admin/js/test/test.js
@@ -0,0 +1,5 @@
+function hello() {
+    alert('hello');
+}
+hello();
+//# sourceMappingURL=C:/github/quickwave/quickwave.admin/quickwave.admin/js/test/test.js.map
---
+++ quickwave.admin/src/test/test.ts
@@ -0,0 +1,6 @@
+﻿
+function hello() {
+    alert('hello');
+}
+
+hello();
